7f275035a4fd553cd65f198b53fe008bcce29c90
tatiana parkhomova <taniz2009@gmail.com>
2024-12-10 21:19:38 +0300

    fix

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,7 +2,7 @@
   <nav class="bg-white border-gray-200 dark:bg-gray-900">
     <div class="flex flex-wrap items-center justify-between mx-auto py-4">
       <a href="http://e436b3cc88eb.vps.myjino.ru/" class="flex items-center space-x-3 rtl:space-x-reverse">
-        <img src="https://e436b3cc88eb.vps.myjino.ru/uploads/cf2ff62cdf9411e29b1586b6a3b862cf_21bf27921f.png" class="h-8"
+        <img src="https://6bc41f582d60.vps.myjino.ru/uploads/cf2ff62cdf9411e29b1586b6a3b862cf_21bf27921f.png" class="h-8"
           alt="Flowbite Logo" />
         <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Психология и саморазвитие</span>
       </a>
